13c8794c71ffa83f01bd6e492fce727c2c5843b8
Abs <65183228+iq0abs@users.noreply.github.com>
2021-07-27 11:46:14 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -95,7 +95,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.write(await resp.read())
                 await f.close()
 
-    image1 = Image.open("./background.png")
+    image1 = Image.open("etc//DevProx.jpg")
     image2 = Image.open("etc/foreground.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
